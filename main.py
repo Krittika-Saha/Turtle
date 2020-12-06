2fd57f2cbf896b87b9262b89c7229d866e4d35e6
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-12-06 16:20:39 +0530

    Challenge 2: Completed!

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from turtle import Turtle, Screen
 import random
 
-timmy_the_turtle = Turtle()
+timmy = Turtle()
 
-timmy_the_turtle.shape('turtle')
+timmy.shape('turtle')
 colors = ['#b088f9', '#bedcfa', '#da9ff9', '#98acf8', '#d35d6e', '#ff8e71', '#fca3cc','#9088d4','#52057b','#006a71','#fcdada','#41aea9','#0d7377']
+directions = [0, 90, 180, 270]
 
-for i in range(3, 10):
-  for j in range(i):
-    timmy_the_turtle.fd(50)
-    timmy_the_turtle.left(360/i)
-    timmy_the_turtle.color(random.choice(colors))
+timmy.pensize(10)
+for i in range(200):
+  timmy.fd(50)
+  timmy.setheading(random.choice(directions))
+  timmy.color(random.choice(colors))
 
 
 
